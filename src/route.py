8bdef962a84d27ad072725b29fec90c02774158b
Fernando Hurtado <fernandohurtado@uc.com>
2023-07-13 16:41:59 -0400

    comentarios listos en route.py

--- src/route.py
+++ src/route.py
@@ -2,102 +2,145 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as signal
 
-def get_new_route(t_total, Fs):
+def get_new_route(t_total, Fs): # entrega un arreglo de ceros, de t_total segundos con una tasa de sampleo Fs
     return np.zeros([int(t_total*Fs)])
 
-def get_rampa(t_init, t_change, delta_v, t_total, Fs=100):
+def get_rampa(t_init, t_change, delta_v, t_total, Fs=100): # entrega una arreglo de t_total segundos con una rampa que empieza en t_init, dura t_change y varia delta_v, con una tasa de sampleo Fs
     return np.pad(np.linspace(0, delta_v, int(round(t_change*Fs, 0))),(int(round(t_init*Fs, 0)), int(round(t_total*Fs,0))-int(round(t_init*Fs,0))-int(round(t_change*Fs, 0))), 'constant', constant_values=(0,delta_v))
 
 def get_route_ramped(points, t_max, Fs=100):
-    ramped = np.zeros([int(t_max*Fs)])
-    if len(points):
+    """
+    A partir de una lista de puntos 
+    points = [[x_0, y_0], [x_1, y_1], ..., [x_N, y_N]]
+    retorna un arreglo de t_max segundos, con una tasa de sampleo Fs, que pasa por los puntos especificados interpolando linealmente entre las muestras
+    """
+    ramped = np.zeros([int(t_max*Fs)]) # partimos con un arreglo vacio
+    if len(points): # Si hay algun punto, en lugar de partir en cero partiremos en el valor del primer punto
         ramped += points[0][1]
-    for i in range(len(points) - 1):
+    for i in range(len(points) - 1): # luego por cada punto adicional sumaremos al arreglo una rampa que lleva desde el punto actual al punto siguiente en linea recta
         ramped += get_rampa(points[i][0], points[i + 1][0] - points[i][0], points[i + 1][1] - points[i][1], t_max)
-    return ramped
+    return ramped 
 
+# definimos un diccionario con funciones para ventanas. Cada una de ellas despues se puede llamar esecificando el numero de muestras. Ej. ventanas['rect'](120)
 ventanas = {'rect': np.ones, 'triangular': np.bartlett, 'blackman': np.blackman, 'hamming': np.hamming, 'hanning': np.hanning, 'kaiser1': lambda x: np.kaiser(x, 1), 'kaiser2': lambda x: np.kaiser(x, 2), 'kaiser3': lambda x: np.kaiser(x, 3), 'kaiser4': lambda x: np.kaiser(x, 4), 'ramp': lambda x: np.linspace(0, 1, x)}
 
-def sum_vibrato(func, t_inicio, t_vibrato, amp, freq, ventana, t_max, Fs=100):
+def sum_vibrato(func, t_inicio, t_vibrato, amp, freq, ventana, t_max, Fs=100): # esta funcion toma un arreglo func y le suma una sinusoide que parte en t_inicio, dura t_vibrato, tiene amplitud amp, frecuencia freq y se ventanea por una ventana a eleccion
     ventana = ventanas[ventana]
     t = np.linspace(0, t_max, int(round(t_max*Fs, 0)))
     return func + np.pad(ventana(int(round(t_vibrato*Fs, 0))), (int(round(t_inicio*Fs)),int(round(t_max*Fs-t_vibrato*Fs-t_inicio*Fs, 0)))) * amp * np.sin(freq*np.pi*t)
 
+### A continuacion creamos varias funciones para aplicar diversos filtros pasabajos a un arreglo f
+### en cada uno los argumentos son:
+# - f: el arreglo a filtrar
+# - numtaps: numero de puntos del filtro
+# - cutoff: frecuencia de corte
+# - Fs: frecuencia de sampleo
+# - Ap: atenuacion en banda pasante
+# - As: Atenuacion de stop
+# - fp: frecuencia pasante
+# - fs: frecuencia stop
+# - rp: ripple
+
+## Los filtros disponibles son:
+## FIR
+##    - Por ventaneo:
+##        * hamming
+##        * hann
+##        * blackman
+##        * bartlett
+##        * rect
+##    - Equiripple
+## IIR
+##    - Butterworth
+##    - Chebyshev 1
+##    - Chebyshev 2
+##    - Elíptico
+
+## Además, como el procesamiento de las señales no es en tiempo real, cada función se ocupa de (una vez filtrada) volver a desplazar la funcion en el desfase de grupo.
+
 ## hamming
 def hamming_filter(f, numtaps, cutoff, Fs):
-    flt = signal.firwin(numtaps=numtaps, cutoff=cutoff, window="hamming", pass_zero="lowpass", fs=Fs)
-    A = [1] +  [0 for i in range(numtaps-1)]
-    w, gd = signal.group_delay((flt, A), fs=Fs)
-    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):]
+    flt = signal.firwin(numtaps=numtaps, cutoff=cutoff, window="hamming", pass_zero="lowpass", fs=Fs) # calculamos el filtro
+    A = [1] +  [0 for i in range(numtaps-1)] 
+    w, gd = signal.group_delay((flt, A), fs=Fs) # calculamos el desfase de grupo
+    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):] # filtramos y desplazamos. Ademas llenamos con los valores iniciales y finales por el desplazamiento
 
 ## hann
 def hann_filter(f, numtaps, cutoff, Fs):
-    flt = signal.firwin(numtaps=numtaps, cutoff=cutoff, window="hann", pass_zero="lowpass", fs=Fs)
+    flt = signal.firwin(numtaps=numtaps, cutoff=cutoff, window="hann", pass_zero="lowpass", fs=Fs) # calculamos el filtro
     A = [1] +  [0 for i in range(numtaps-1)]
-    w, gd = signal.group_delay((flt, A), fs=Fs)
-    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):]
+    w, gd = signal.group_delay((flt, A), fs=Fs) # calculamos el desfase de grupo
+    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):] # filtramos y desplazamos. Ademas llenamos con los valores iniciales y finales por el desplazamiento
 
 ## blackman
 def blackman_filter(f, numtaps, cutoff, Fs):
-    flt = signal.firwin(numtaps=numtaps, cutoff=cutoff, window="blackman", pass_zero="lowpass", fs=Fs)
+    flt = signal.firwin(numtaps=numtaps, cutoff=cutoff, window="blackman", pass_zero="lowpass", fs=Fs) # calculamos el filtro
     A = [1] +  [0 for i in range(numtaps-1)]
-    w, gd = signal.group_delay((flt, A), fs=Fs)
-    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):]
+    w, gd = signal.group_delay((flt, A), fs=Fs) # calculamos el desfase de grupo
+    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):] # filtramos y desplazamos. Ademas llenamos con los valores iniciales y finales por el desplazamiento
 
 ## bartlett
 def bartlett_filter(f, numtaps, cutoff, Fs):
-    flt = signal.firwin(numtaps=numtaps, cutoff=cutoff, window="bartlett", pass_zero="lowpass", fs=Fs)
+    flt = signal.firwin(numtaps=numtaps, cutoff=cutoff, window="bartlett", pass_zero="lowpass", fs=Fs) # calculamos el filtro
     A = [1] +  [0 for i in range(numtaps-1)]
-    w, gd = signal.group_delay((flt, A), fs=Fs)
-    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):]
+    w, gd = signal.group_delay((flt, A), fs=Fs) # calculamos el desfase de grupo
+    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):] # filtramos y desplazamos. Ademas llenamos con los valores iniciales y finales por el desplazamiento
 
 ## rect
 def rect_filter(f, numtaps, cutoff, Fs):
-    flt = signal.firwin(numtaps=numtaps, cutoff=cutoff, window="boxcar", pass_zero="lowpass", fs=Fs)
+    flt = signal.firwin(numtaps=numtaps, cutoff=cutoff, window="boxcar", pass_zero="lowpass", fs=Fs) # calculamos el filtro
     A = [1] +  [0 for i in range(numtaps-1)]
-    w, gd = signal.group_delay((flt, A), fs=Fs)
-    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):]
+    w, gd = signal.group_delay((flt, A), fs=Fs) # calculamos el desfase de grupo
+    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):] # filtramos y desplazamos. Ademas llenamos con los valores iniciales y finales por el desplazamiento
 
 def remez_filter(f, Ap, As, fp, fs, Fs):
+    # primero debemos calcular el orden del filtro:
     delta_p = (10**(Ap/20)-1)/(10**(Ap/20)+1)
     delta_s = (1+delta_p)/(10**(As/20))
     n = int(np.ceil((-20*np.log10(np.sqrt(delta_p*delta_s))-13)/(2.324*((fs-fp)/Fs)*2*np.pi)+1))
     if not n%2:
         n+=1
-    print(n)
-    flt = signal.remez(n, [0, fp, fs, 0.5*Fs], [1, 0], fs=Fs)
+    flt = signal.remez(n, [0, fp, fs, 0.5*Fs], [1, 0], fs=Fs) # calculamos el filtro
     A = [1] +  [0 for i in range(n-1)]
-    w, gd = signal.group_delay((flt, A), fs=Fs)
-    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):]
+    w, gd = signal.group_delay((flt, A), fs=Fs) # calculamos el desfase de grupo
+    return signal.lfilter(flt, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):] # filtramos y desplazamos. Ademas llenamos con los valores iniciales y finales por el desplazamiento
 
 def butter_filter(f, Ap, As, fp, fs, Fs):
-    N, Wn = signal.buttord(fp, fs, Ap, As, fs=Fs)
-    B, A = signal.butter(N, Wn, btype='low', fs=Fs)
-    w, gd = signal.group_delay((B, A), fs=Fs)
-    return signal.lfilter(B, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):]
+    N, Wn = signal.buttord(fp, fs, Ap, As, fs=Fs) # calculamos el orden necesario
+    B, A = signal.butter(N, Wn, btype='low', fs=Fs) # calculamos el filtro
+    w, gd = signal.group_delay((B, A), fs=Fs) # calculamos el desfase de grupo
+    return signal.lfilter(B, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):] # filtramos y desplazamos. Ademas llenamos con los valores iniciales y finales por el desplazamiento
 
 ## cheb1
 def cheb1_filter(f, Ap, As, fp, fs, rp, Fs):
-    n, wn = signal.cheb1ord(fp, fs, Ap, As, fs=Fs)
-    B, A  = signal.cheby1(n, rp, wn, 'low', fs=Fs)
-    w, gd = signal.group_delay((B, A), fs=Fs)
-    return signal.lfilter(B, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):]
+    n, wn = signal.cheb1ord(fp, fs, Ap, As, fs=Fs) # calculamos el orden necesario
+    B, A  = signal.cheby1(n, rp, wn, 'low', fs=Fs) # calculamos el filtro
+    w, gd = signal.group_delay((B, A), fs=Fs) # calculamos el desfase de grupo
+    return signal.lfilter(B, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):] # filtramos y desplazamos. Ademas llenamos con los valores iniciales y finales por el desplazamiento
 
 ## cheb2
 def cheb2_filter(f, Ap, As, fp, fs, rp, Fs):
-    n, wn = signal.cheb2ord(fp, fs, Ap, As, fs=Fs)
-    B, A  = signal.cheby2(n, rp, wn, 'low', fs=Fs)
-    w, gd = signal.group_delay((B, A), fs=Fs)
-    return signal.lfilter(B, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):]
+    n, wn = signal.cheb2ord(fp, fs, Ap, As, fs=Fs) # calculamos el orden necesario
+    B, A  = signal.cheby2(n, rp, wn, 'low', fs=Fs) # calculamos el filtro
+    w, gd = signal.group_delay((B, A), fs=Fs) # calculamos el desfase de grupo
+    return signal.lfilter(B, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):] # filtramos y desplazamos. Ademas llenamos con los valores iniciales y finales por el desplazamiento
 
 ## elliptic
 def ellip_filter(f, Ap, As, fp, fs, Fs):
-    n, wn = signal.ellipord(fp, fs, Ap, As, fs=Fs)
-    B, A  = signal.ellip(n, Ap, As, wn, 'low', fs=Fs)
-    w, gd = signal.group_delay((B, A), fs=Fs)
-    return signal.lfilter(B, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):]
+    n, wn = signal.ellipord(fp, fs, Ap, As, fs=Fs) # calculamos el orden necesario
+    B, A  = signal.ellip(n, Ap, As, wn, 'low', fs=Fs) # calculamos el filtro
+    w, gd = signal.group_delay((B, A), fs=Fs) # calculamos el desfase de grupo
+    return signal.lfilter(B, A, np.pad(f, (int(gd[0]), int(gd[0])), 'constant', constant_values=(f[0], f[-1])))[int(2*int(gd[0])):] # filtramos y desplazamos. Ademas llenamos con los valores iniciales y finales por el desplazamiento
 
 def filter_func(f, filter, params, Fs):
+    """
+    Dado un arreglo f, especificando un filtro filter y sus parametros params (con una frecuencia de sampleo Fs), esta funcion retorna la el arreglo filtrado.
+    filter puede tomar los valores ["firwin", "remez", "butter", "chebyshev", "elliptic"]
+    de acuerdo a la seleccion de filtros, los parametros necesarios cambian.
+    En el caso de firwin los params son [window, n, cutoff]
+    En el caso de remez, butter o elliptic los params son [Ap, As, fp, fs]
+    En el caso de chebyshev los params son [n, Ap, As, fp, fs, rp]
+    """
     if filter == "firwin":
         '''
         params = [window, n, cutoff]
@@ -162,18 +205,22 @@ def filter_func(f, filter, params, Fs):
         raise Exception("Filter not implemented, try with firwin, remez, butter, chebyshev or elliptic")
     
 def filter_part_func(f, i_init, i_end, filter, params, Fs):
+    """
+    Esta funcion toma un arreglo f, y le aplica un filtro solo entre i_init y i_end.
+    Luego retorna el filtro concatenando las partes no filtradas con la filtrada 
+    """
     f_sub = f[int(round(i_init*Fs, 0)):int(round(i_end*Fs, 0))]
     filtered = filter_func(f_sub, filter, params, Fs)
     f[int(round(i_init*Fs, 0)):int(round(i_end*Fs, 0))] = filtered
     return f
 
-def lengthen_func(f, new_t_max, Fs):
+def lengthen_func(f, new_t_max, Fs): # toma una arreglo f y lo acorta hasta que tiene un largo new_t_max*Fs
     return np.linspace(0, new_t_max, int(round(new_t_max*Fs, 0))), np.hstack([f, f[-1]*np.ones([int(round(new_t_max*Fs, 0))-len(f)])])
 
-def shorten_func(f, new_t_max, Fs):
+def shorten_func(f, new_t_max, Fs): # toma un arreglo f y lo alarga hasta que tiene un largo new_t_max*Fs
     return np.linspace(0, new_t_max, int(round(new_t_max*Fs, 0))), f[:int(round(new_t_max*Fs, 0))]
 
-def change_duration(f, new_t_max, Fs):
+def change_duration(f, new_t_max, Fs): # toma un arreglo f y le cambia su duracion hasta new_t_max. Acortandolo o alargandolo segun corresponda
     if new_t_max > len(f)*Fs:
         return lengthen_func(f, new_t_max, Fs)
     else:
@@ -206,10 +253,10 @@ def calculate_route(route):
     points = route['points']
     vibratos = route['vibrato']
     filters = route['filters']
-    f = get_new_route(t_total, Fs)
-    f = get_route_ramped(points, t_total, Fs=Fs)
+    f = get_new_route(t_total, Fs) # primero creamos una lista vacia
+    f = get_route_ramped(points, t_total, Fs=Fs) # luego interpolamos linealmente entre los puntos definidos
     fil_t = []
-    for filt in filters:
+    for filt in filters: # aplicamos cada uno de los filtros
         i_init = filt[0]
         i_end = filt[1]
         filter = filt[2]
@@ -217,7 +264,7 @@ def calculate_route(route):
         fil_t.append(i_init)
         f = filter_part_func(f, i_init, i_end, filter, params, Fs)
     vib_t = []
-    for vib in vibratos:
+    for vib in vibratos: # sumamos todos los vibratos
         t_init = vib[0]
         t_vibrato = vib[1]
         amp = vib[2]
@@ -225,48 +272,52 @@ def calculate_route(route):
         window = vib[4]
         vib_t.append(t_init)
         f = sum_vibrato(f, t_init, t_vibrato, amp, freq, window, t_total, Fs=Fs)
-    t = np.linspace(0, t_total, int(round(t_total*Fs, 0)))
-    return t, f, points, vib_t, fil_t
+    t = np.linspace(0, t_total, int(round(t_total*Fs, 0))) # ademas creamos un vector de tiempo
+    return t, f, points, vib_t, fil_t # y retornamos el vector de tiempo, la funcion final, la lista de puntos por los que pasa, los tiempos de inicio de cada vibrato y los puntos de inicio de cada filtro
 
+## estos diccionarios los usamos para traducir las notas a numeros y viceversa. Sirve para graficarlos en el plano y para interpretar sobre que nota se hace click cuando el usuario interactua con el grafico de las notas
 dict_notes = {0.0: 'D3', 0.5: 'D#3', 1.0: 'E3', 1.5: 'F3', 2.0: 'F#3', 2.5: 'G3', 3.0: 'G#3', 3.5: 'A3', 4.0: 'A#3', 4.5: 'B3', 5.0: 'C4', 5.5: 'C#4', 6.0: 'D4', 6.5: 'D#4', 7.0: 'E4', 7.5: 'F4', 8.0: 'F#4', 8.5: 'G4', 9.0: 'G#4', 9.5: 'A4', 10.0: 'A#4', 10.5: 'B4', 11.0: 'C5', 11.5: 'C#5', 12.0: 'D5', 12.5: 'D#5', 13.0: 'E5', 13.5: 'F5', 14.0: 'F#5', 14.5: 'G5', 15.0: 'G#5', 15.5: 'A5', 17.0: 'C6'}
-dict_notes_rev = {}
+dict_notes_rev = {} # creamos el diccionario inverso
 for key, item in dict_notes.items():
     dict_notes_rev[item] = key
 
 def calculate_notes_route(route):
+    """
+    Una funcion parecida a calculate_route, solo que en el caso de las notas hay unas pocas diferencias que la distinguen.
+    - En lugar de hacer una interpolacion lineal entre muestras, se hace un zero order hold. Es decir, se mantiene una nota hasta que se cambia a la siguiente
+    - En lugar de vibrato, se permite agregar trills (con saltos discretos entre notas)
+    - No se permite filtrar
+    """
     t_total = route['total_t']
     Fs = route['Fs']
     notes = route['notes']
     trills = route['trill']
     t = np.linspace(0, t_total, int(round(t_total*Fs, 0)))
-    f = get_new_route(t_total, Fs)
+    f = get_new_route(t_total, Fs) # partimos con un arreglo se ceros
     tr_x = []
     tr_y = []
-    for tr in trills:
-        # [x, note, freq, duration]
+    for tr in trills: # partimos sumando los trills. Estos tienen de parámetro [t, note, freq, duration]
         ti = tr[0]
         dist = tr[1]
         freq = tr[2]
         duration = tr[3]
-        #print(tr)
         for i in range(int(freq*duration)):
-            f = f + np.heaviside(t - ti - 2*i/(2*freq), 1) * dist - np.heaviside(t - ti - (2*i+1)/(2*freq), 1) * dist
+            f = f + np.heaviside(t - ti - 2*i/(2*freq), 1) * dist - np.heaviside(t - ti - (2*i+1)/(2*freq), 1) * dist # creamos el trill y se lo sumamos a la funcion como una serie de escalones
 
     x_points = []
     y_points = []
-    if len(notes):
+    if len(notes): # si hay alguna nota, partimos de esta nota (la sumamos como offset)
         alt_i = dict_notes_rev[notes[0][1]]
         f += alt_i
-    for n in notes:
+    for n in notes: # ahora nos encargamos de pasar por los puntos haciendo el ZO
         x_points.append(n[0])
         y_points.append(dict_notes_rev[n[1]])
-        #print(type(np.heaviside(t - n[0], 1)), type(dict_notes_rev[n[1]] - alt_i))
-        f += np.heaviside(t - n[0], 1) * (dict_notes_rev[n[1]] - alt_i)
+        f += np.heaviside(t - n[0], 1) * (dict_notes_rev[n[1]] - alt_i) # sumamos los escalones
         alt_i = dict_notes_rev[n[1]]
     
     for tr in trills:
         tr_x.append(tr[0])
         tr_y.append(f[int(tr[0]*len(f)/t_total)]-0.1)
 
-    return t, f, x_points, y_points, tr_x, tr_y
+    return t, f, x_points, y_points, tr_x, tr_y # y retornamos el vector de tiempo, la funcion final, las listas de las posiciones de los tiempos y las listas de las posiciones de los trills
 
